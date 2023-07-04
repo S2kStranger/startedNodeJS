93314c7f2f6e386c374cf8ccc5253fd7c1b4acf8
S2kStranger <s2kkasera@gmail.com>
2023-07-04 20:22:00 +0530

    created server , added req and res with ifs

--- app.js
+++ app.js
@@ -1,5 +1,30 @@
 const http=require('http');
 const server=http.createServer((req,res) => {
-    console.log('Shivam');
+    const url=req.url;
+    if(url==='/home')
+    {
+        res.write('<html>');
+        res.write('<head><title>My first page</title></head>');
+        res.write('<body><h1>Welcome home</h1></body>');
+        res.write('</html>');
+        return res.end();
+    }
+    else if(url==='/about')
+    {
+        res.write('<html>');
+        res.write('<head><title>My first page</title></head>');
+        res.write('<body><h1>Welcome to About Us page</h1></body>');
+        res.write('</html>');
+        return res.end();
+    }
+    else if(url==='/node')
+    {
+        res.write('<html>');
+        res.write('<head><title>My first page</title></head>');
+        res.write('<body><h1>Welcome to my Node Js project</h1></body>');
+        res.write('</html>');
+        return res.end();
+    }
+    
 });
 server.listen(4000);
