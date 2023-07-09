fe29705e7de7cbe154fe408a16ea30610f5b65b2
S2kStranger <s2kkasera@gmail.com>
2023-07-09 17:20:13 +0530

    Intro to Express.JS(Used two middleware)

--- app-file.js
+++ app-file.js
@@ -1,4 +1,14 @@
-const http=require('http');
-const route=require('./route.js');
-const server=http.createServer(route.handler);
-server.listen(4000);
+const express=require('express');
+const app=express();
+
+app.use((req,res,next) => {
+    console.log('In the middleware!');
+    next(); //Allows the context to continue to the next middleware in line
+});
+
+app.use((req,res,next) => {
+    console.log('In another middleware!');
+    res.send('<h1>Hello from express JS</h1>');
+});
+
+app.listen(3000);
