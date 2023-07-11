0e8b5af8f1a40bac82d68b4412c7d3f7605bb1b0
S2kStranger <s2kkasera@gmail.com>
2023-07-11 23:07:43 +0530

    Parsing the data

--- app-file.js
+++ app-file.js
@@ -1,14 +1,22 @@
 const express=require('express');
-const app=express();
 
-app.use((req,res,next) => {
-    console.log('In the middleware!');
-    next(); //Allows the context to continue to the next middleware in line
+
+const bodyParser=require('body-parser');
+const app=express();
+app.use(bodyParser.urlencoded({extended:false}));
+
+app.use('/add-product',(req,res,next) => {
+    res.send('<form action = "/product" method="POST"> <label>Product:</label><input type="text" name="title"><label> Size :</label><input type="text" name="size"><button type="submit">Send</button></form>' );
+
 });
 
-app.use((req,res,next) => {
-    console.log('In another middleware!');
-    res.send('<h1>Hello from express JS</h1>');
+app.use('/product', (req,res,next) => {
+    console.log(req.body);
+    return res.redirect('/');
+});
+
+app.use('/', (req,res,next) => {
+    res.send('<h1>Hello from Express!</h1>');
 });
 
 app.listen(3000);
